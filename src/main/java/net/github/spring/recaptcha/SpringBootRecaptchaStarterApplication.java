e9341f1fd923cb0ad975e5fe4e1221cd5f756299
Sachin Gorade <sachin.gorade@gmail.com>
2020-12-17 21:25:12 +0100

    Starter project commit

---
+++ src/main/java/net/github/spring/recaptcha/SpringBootRecaptchaStarterApplication.java
@@ -0,0 +1,13 @@
+package net.github.spring.recaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecaptchaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecaptchaStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/github/spring/recaptcha/SpringBootRecaptchaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package net.github.spring.recaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRecaptchaStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
